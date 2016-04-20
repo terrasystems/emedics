0e93fd1404920f1b924a52b52f5beb2be4f5a109
Valik Novikov <walian19947@gmail.com>
2016-04-20 13:52:52 +0300

    add bootstrap, give login form some styles

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,14 @@
 
 /* App Module */
 
-var eMedics = angular.module('eMedics', ['ui.router', 'modules.core', 'modules.public', 'ui.bootstrap','formly', 'formlyBootstrap']);
+var eMedics = angular.module('eMedics', ['ui.router', 'modules.core', 'modules.public', 'ui.bootstrap', 'formly', 'formlyBootstrap']);
 
-eMedics.config(function( $statesList, $stateProvider, $urlRouterProvider) {
+eMedics.config(function( statesList, $stateProvider, $urlRouterProvider) {
 
 	$stateProvider.state('login222', {
 		url: '/login',
 		templateUrl: 'modules/public/views/login.html',
-		controller: 'LoginCtrl'
+		controller: 'LoginCtrl as vm'
 	});
 	$urlRouterProvider.otherwise('login');
 })
