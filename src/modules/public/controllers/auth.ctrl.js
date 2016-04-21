08a3d4ad0e6ba9911a7d27d6668623446d69ecf4
domino55 <domino55@terrasystems.eu>
2016-04-21 15:48:00 +0300

    Merge branch 'master' of D:\WORK\Upwork\ng-pouchdb\emedics-web with conflicts.

--- src/modules/public/controllers/auth.ctrl.js
+++ src/modules/public/controllers/auth.ctrl.js
@@ -3,7 +3,8 @@
 angular.module('modules.public', [])
 
 
-	.controller('LoginCtrl', function ($rootScope, $scope, $state) {
+.controller('LoginCtrl', function($rootScope, $scope, $state)
+	{
 		console.log('..LoginCtrl');
 		var vm = this;
 		vm.onSubmit = onSubmit;
@@ -38,197 +39,29 @@ angular.module('modules.public', [])
 		function onSubmit() {
 			console.log('submit');
 			if (vm.form.$valid) {
+			   console.log('..Ok!');
+			}
+		}
+	})
+
+
+.controller('Registration', function($rootScope, $scope, $state)
+	{
+		console.log('..Registration');
+		var vm = this;
+		vm.onSubmit = onSubmit;
+
+		function onSubmit() {
+			console.log('submit');
+			if (vm.form.$valid) {
 				console.log('..Ok!');
 			}
 		}
 	})
 
 
-	.controller('Registration', function ($rootScope, $scope, $state) {
-		console.log('..Registration');
-		var vm = this;
-
-		vm.model = {};
-
-		vm.tabs = [
-			{
-
-				title: 'Patient',
-				type: 'Patient',
-				active: true,
-				form: {
-					options: {},
-					model: vm.model,
-					fields: [
-						{
-							className: 'col-md-6',
-							key: 'firstName',
-							type: 'input',
-							templateOptions: {
-								label: 'First Name',
-								required: true,
-								placeholder:'Enter your First Name'
-							}
-
-						},
-						{
-							className: 'col-md-6',
-							key: 'lastName',
-							type: 'input',
-							templateOptions: {
-								label: 'Last Name',
-								required: true,
-								placeholder:'Enter your Last Name'
-							}
-
-						},
-						{
-							className: 'col-md-6',
-							key: 'email',
-							type: 'input',
-							templateOptions: {
-								type: 'email',
-								required: true,
-								label: 'Email address',
-								placeholder: 'Enter email'
-							}
-
-						},
-						{
-							className: 'col-md-6',
-							key: '',
-							type: 'input',
-							templateOptions: {
-								type: 'password',
-								required: true,
-								label: 'Password',
-								placeholder: 'Enter password'
-							}
-
-						}
-
-					]
-				}
-			},
-			{
-				title: 'Doctor',
-				type: 'Doctor',
-				form: {
-					model: vm.model,
-					fields: [
-						{
-							className: 'col-md-6',
-							key: 'firstName',
-							type: 'input',
-							templateOptions: {
-								label: 'First Name',
-								required: true,
-								placeholder:'Enter your First Name'
-							}
-
-						},
-						{
-							className: 'col-md-6',
-							key: 'lastName',
-							type: 'input',
-							templateOptions: {
-								label: 'Last Name',
-								required: true,
-								placeholder:'Enter your Last Name'
-							}
-
-						},
-						{
-							className: 'col-md-6',
-							key: 'email',
-							type: 'input',
-							templateOptions: {
-								type: 'email',
-								required: true,
-								label: 'Email address',
-								placeholder: 'Enter email'
-							}
-
-						},
-						{
-							className: 'col-md-6',
-							key: '',
-							type: 'input',
-							templateOptions: {
-								type: 'password',
-								required: true,
-								label: 'Password',
-								placeholder: 'Enter password'
-							}
-
-						}
-					]
-				}
-			},
-			{
-				title: 'Homecare Organization',
-				type:'Homecare Organization',
-				active: true,
-				form: {
-					options: {},
-					model: vm.model,
-					fields: [
-						{
-							className: 'col-md-6',
-							key: 'firstName',
-							type: 'input',
-							templateOptions: {
-								label: 'First Name',
-								required: true,
-								placeholder:'Enter your First Name'
-							}
-
-						},
-						{
-							className: 'col-md-6',
-							key: 'lastName',
-							type: 'input',
-							templateOptions: {
-								label: 'Last Name',
-								required: true,
-								placeholder:'Enter your Last Name'
-							}
-
-						},
-						{
-							className: 'col-md-6',
-							key: 'email',
-							type: 'input',
-							templateOptions: {
-								type: 'email',
-								required: true,
-								label: 'Email address',
-								placeholder: 'Enter email'
-							}
-
-						},
-						{
-							className: 'col-md-6',
-							key: '',
-							type: 'input',
-							templateOptions: {
-								type: 'password',
-								required: true,
-								label: 'Password',
-								placeholder: 'Enter password'
-							}
-
-						}
-
-					]
-				}
-			}
-		];
-
-	})
-
-
-	.controller('NewPassword', function ($rootScope, $scope, $state) {
+.controller('NewPassword', function($rootScope, $scope, $state)
+	{
 		console.log('..NewPassword');
 		var vm = this;
 		vm.onSubmit = onSubmit;
