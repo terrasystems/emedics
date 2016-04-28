b5178c019734fe71b5ef16a10adb39203e9ce759
faust <faust@terrasystems.eu>
2016-04-28 19:02:09 +0300

    T729 change fields notifications

--- src/modules/dashboard/controllers/dashboard.patNotif.ctrl.js
+++ src/modules/dashboard/controllers/dashboard.patNotif.ctrl.js
@@ -6,9 +6,9 @@ angular.module('modules.dash')
 	.controller('patientNotifCtrl', function ($state) {
 		//console.log('..patientNotifCtrl');
 		var vm = this;
-		vm.notifications = [{name: 'Vasia', secondName: 'Piatochkin', msg: 'a ja jebu sobak'},
-			{name: 'Vasia', secondName: 'Piatochkin', msg: 'a ja jebu sobak'},
-			{name: 'Vasia', secondName: 'Piatochkin', msg: 'a ja jebu sobak'},
-			{name: 'Vasia', secondName: 'Piatochkin', msg: 'a ja jebu sobak'}];
+		vm.notifications = [{name: 'Klod', secondName: 'Walkin', msg: 'Check my form #1'},
+			{name: 'Fill', secondName: 'Manison', msg: 'I fill form #12'},
+			{name: 'Andrew', secondName: 'Frolow', msg: 'Change info'},
+			{name: 'Derick', secondName: 'Wolf', msg: 'Can you check info'}];
 	}
 );
