513436cbdd421f3f8cc5c29835a0bbd9fab426dc
Valik Novikov <walian19947@gmail.com>
2016-04-28 12:25:20 +0300

    T731 change styles

--- src/modules/dashboard/controllers/dashboard.patForms.crtl.js
+++ src/modules/dashboard/controllers/dashboard.patForms.crtl.js
@@ -6,7 +6,7 @@ angular.module('modules.patforms', ['ui-listView'])
 .controller('DashboardPatFormsCtrl', function($rootScope, $state, $http,constants,$stateParams)  {
 		console.log('..DashboardPatFormsCtrl!!!!');
 		var vm = this;
-    //vm.list=constants.listTasks;
+    vm.list=constants.listTasks;
     vm.list =$stateParams.activeForms;
 		//
 		//$http.post('rest/dashboard/forms/active', {first:1,count:20}).then(function (resp) {
