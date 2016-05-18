ef59eeb11b04cf02fd97bb4173526c22885be178
domino55 <domino55@terrasystems.eu>
2016-05-18 18:17:37 +0300

    T765 registratio

--- src/modules/core/config/core.reg_fields.js
+++ src/modules/core/config/core.reg_fields.js
@@ -3,183 +3,151 @@
 
 angular.module('modules.core')
 
-	.constant('reg_fields', [
+.constant('pat_fields', [
 		{
-			title: 'Patient',
-			active: true,
-			index : 0,
-			type: 'pat',
-			form: {
-				options: {},
-				model: undefined,
-				fields: [
-					{
-						className: 'col-md-12',
-						key: 'pat.user.username',
-						type: 'input',
-						templateOptions: {
-							label: 'Name',
-							required: false,
-							placeholder: 'Enter your Name'
-						}
-					},
-					{
-						className: 'col-md-12',
-						key: 'pat.user.email',
-						type:'input',
-						templateOptions: {
-							type: 'email',
-							required: false,
-							label: 'Email address',
-							placeholder: 'Enter email'
-						}
-					},
-					{
-						className: 'col-md-12',
-						key: 'pat.user.password',
-						type: 'input',
-						templateOptions: {
-							type: 'password',
-							required: false,
-							label: 'Password',
-							placeholder: 'Enter password'
-						}
-					}
-				]
+			className: 'col-md-12',
+			key: 'user.username',
+			type: 'input',
+			templateOptions: {
+				label: 'Name',
+				required: true,
+				placeholder: 'Enter your Name'
 			}
 		},
 		{
-			title: 'Doctor',
-			active: false,
-			index : 1,
-			type: 'doc',
-			form: {
-				model: undefined,
-				fields: [
-					{
-						className: 'col-md-12',
-						key: 'doc.user.username',
-						type: 'input',
-						templateOptions: {
-							label: 'Name',
-							required: false,
-							placeholder: 'Enter your Name'
-						}
-					},
-					{
-						className: 'col-md-12',
-						key: 'doc.user.email',
-						type: 'input',
-						templateOptions: {
-							type: 'email',
-							required: false,
-							label: 'Email address',
-							placeholder: 'Enter email'
-						}
-					},
-					{
-						className: 'col-md-12',
-						key: 'doc.user.password',
-						type: 'input',
-						templateOptions: {
-							type: 'password',
-							required: false,
-							label: 'Password',
-							placeholder: 'Enter password'
-						}
-					}
-				]
+			className: 'col-md-12',
+			key: 'user.email',
+			type:'input',
+			templateOptions: {
+				type: 'email',
+				required: true,
+				label: 'Email address',
+				placeholder: 'Enter email'
 			}
 		},
 		{
-			title: 'Homecare Organization',
-			active: false,
-			index : 2,
-			type: 'org',
-			form: {
-				options: {},
-				model: undefined,
-				fields: [
-					{
-						className: 'col-md-12',
-						key: 'org.user.username',
-						type: 'input',
-						templateOptions: {
-							label: 'Name',
-							required: false,
-							placeholder: 'Enter your Name'
-						}
-					},
-					{
-						className: 'col-md-12',
-						key: 'org.user.email',
-						type: 'input',
-						templateOptions: {
-							type: 'email',
-							required: false,
-							label: 'Email address',
-							placeholder: 'Enter email'
-						}
-					},
-					{
-						className: 'col-md-12',
-						key: 'org.user.password',
-						type: 'input',
-						templateOptions: {
-							type: 'password',
-							required: false,
-							label: 'Password',
-							placeholder: 'Enter password'
-						}
-					},
-					{
-						className: 'col-md-12',
-						key: 'org.org.www',
-						type: 'input',
-						templateOptions: {
-							label: 'website',
-							required: false,
-							placeholder: 'Enter your web-site'
-						}
-					},
-					{
-						className: 'col-md-12',
-						key: 'org.org.fullname',
-						type: 'input',
-						templateOptions: {
-							label: 'Organization name',
-							required: false,
-							placeholder: 'Full organization name'
-						}
-					},
-					{
-						className: 'col-md-12',
-						key: 'org.org.address',
-						type: 'input',
-						templateOptions: {
-							label: 'Address',
-							required: false,
-							placeholder: 'Organization Address'
-						}
-					},
-					{
-						className: 'col-md-12',
-						key: 'org.org.type',
-						type: 'select',
-						templateOptions: {
-							label: 'Type organization',
-							required: false,
-							placeholder: 'Type organization',
-							options: [
-								{name: 'Type #1', value: 'Type #1'},
-								{name: 'Type #2', value: 'Type #2'},
-								{name: 'Type #3', value: 'Type #3'}
-								]
-						}
-					}
+			className: 'col-md-12',
+			key: 'user.password',
+			type: 'input',
+			templateOptions: {
+				type: 'password',
+				required: true,
+				label: 'Password',
+				placeholder: 'Enter password'
+			}
+		}
+	])
+
+.constant('doc_fields', [
+		{
+			className: 'col-md-12',
+			key: 'user.username',
+			type: 'input',
+			templateOptions: {
+				label: 'Name',
+				required: true,
+				placeholder: 'Enter your Name'
+			}
+		},
+		{
+			className: 'col-md-12',
+			key: 'user.email',
+			type: 'input',
+			templateOptions: {
+				type: 'email',
+				required: true,
+				label: 'Email address',
+				placeholder: 'Enter email'
+			}
+		},
+		{
+			className: 'col-md-12',
+			key: 'user.password',
+			type: 'input',
+			templateOptions: {
+				type: 'password',
+				required: true,
+				label: 'Password',
+				placeholder: 'Enter password'
+			}
+		}
+	])
+.constant('org_fields', [
+		{
+			className: 'col-md-12',
+			key: 'user.username',
+			type: 'input',
+			templateOptions: {
+				label: 'Name',
+				required: true,
+				placeholder: 'Enter your Name'
+			}
+		},
+		{
+			className: 'col-md-12',
+			key: 'user.email',
+			type: 'input',
+			templateOptions: {
+				type: 'email',
+				required: true,
+				label: 'Email address',
+				placeholder: 'Enter email'
+			}
+		},
+		{
+			className: 'col-md-12',
+			key: 'user.password',
+			type: 'input',
+			templateOptions: {
+				type: 'password',
+				required: true,
+				label: 'Password',
+				placeholder: 'Enter password'
+			}
+		},
+		{
+			className: 'col-md-12',
+			key: 'org.www',
+			type: 'input',
+			templateOptions: {
+				label: 'website',
+				required: true,
+				placeholder: 'Enter your web-site'
+			}
+		},
+		{
+			className: 'col-md-12',
+			key: 'org.fullname',
+			type: 'input',
+			templateOptions: {
+				label: 'Organization name',
+				required: true,
+				placeholder: 'Full organization name'
+			}
+		},
+		{
+			className: 'col-md-12',
+			key: 'org.address',
+			type: 'input',
+			templateOptions: {
+				label: 'Address',
+				required: true,
+				placeholder: 'Organization Address'
+			}
+		},
+		{
+			className: 'col-md-12',
+			key: 'org.type',
+			type: 'select',
+			templateOptions: {
+				label: 'Type organization',
+				required: true,
+				placeholder: 'Type organization',
+				options: [
+					{name: 'Type #1', value: 'Type #1'},
+					{name: 'Type #2', value: 'Type #2'},
+					{name: 'Type #3', value: 'Type #3'}
 				]
 			}
 		}
-	]
-
-);
+	]);
