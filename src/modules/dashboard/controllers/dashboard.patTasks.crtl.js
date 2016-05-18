7fc8c85b5b5e34502d28a8624c677dc93acaf14a
domino55 <domino55@terrasystems.eu>
2016-05-18 11:19:02 +0300

    T731 refs add button Apply in select

--- src/modules/dashboard/controllers/dashboard.patTasks.crtl.js
+++ src/modules/dashboard/controllers/dashboard.patTasks.crtl.js
@@ -11,6 +11,9 @@ angular.module('modules.dash')
 		vm.list = [];
 		vm.user = localStorageService.get('userData');
 		//vm.list = $stateParams.activeForms;
+		vm.sections = [];
+		vm.options = [];
+		vm.sectionsName = [];
 
 		var paramsPOST = {"page": {"start": 0, "count": 20}, "criteria": {}};
 
@@ -23,11 +26,28 @@ angular.module('modules.dash')
 				}
 			});
 
+		vm.onGo = function (index, name) {
+			console.log('id=' + index + ' section=' + name);
+			$state.go('main.private.dashboard.tasks.edit', {id: index, section: name});
+		};
 
 		vm.onClick = function (index) {
 			console.log(index + ' !!!!');
-			$state.go('main.private.dashboard.tasks.edit', {id: index});
+			var paramsPOST = {};
+			http.get('private/dashboard/' + vm.user.type + '/forms/' + index, paramsPOST)
+				.then(function (res) {
+					blockUI.stop();
+					if (res.result && res.result.blank && res.result.blank.body &&	res.result.blank.body.sections && angular.isArray(res.result.blank.body.sections) && res.result.id ) {
+						vm.model = (res.result.data && res.result.data.sections) ? res.result.data.sections : {};
+						vm.sectionsName = [];
+						res.result.blank.body.sections.forEach(function(item){
+							vm.sectionsName.push(Object.keys(item)[0]);
+						});
+
+					}
+				});
 		};
+
 
 	}
 );
