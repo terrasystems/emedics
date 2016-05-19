2900756c2dc323610c6083e04f303c9cb87df22e
domino55 <domino55@terrasystems.eu>
2016-05-19 18:25:26 +0300

    notif list change + fix refs select

--- src/modules/dashboard/controllers/dashboard.patNotif.ctrl.js
+++ src/modules/dashboard/controllers/dashboard.patNotif.ctrl.js
@@ -6,13 +6,63 @@ angular.module('modules.dash')
 	.controller('patientNotifCtrl', function (http, blockUI, initParamsPOST) {
 		var vm = this;
 		vm.UnreadNotifications = [];
+		vm.searchnotif = '';
+
+		vm.UnreadNotifications = [
+		{
+			'id': '1212',
+			'date': new Date(),
+			'readtype': 'true',
+			'type': '1',
+			'title': 'Test Title',
+			'text': 'Test Text',
+			'fromId': '4444',
+			'toId': '333',
+			'userForm': '2222'
+		},
+		{
+			'id': '1818',
+			'date': new Date(),
+			'readtype': 'true',
+			'type': '1',
+			'title': 'Test Title #2',
+			'text': 'Test Text #2',
+			'fromId': '5555',
+			'toId': '333',
+			'userForm': '2222'
+		}
+		];
 
 		http.post('private/dashboard/notifications', initParamsPOST.params)
 			.then(function (res) {
 				blockUI.stop();
 				if  (res.result) {
-					vm.UnreadNotifications = res.result;
+					if  (angular.isArray(res.result) && res.result.length>0) {
+						vm.UnreadNotifications = res.result;
+					}
 				}
 			});
+
+		vm.onAccept = function (id) {
+			console.log('accept id: '+id);
+			http.get('private/dashboard/notifications/accept/'+id)
+				.then(function (res) {
+					blockUI.stop();
+					if (res.state) {
+						alertService.add(0, res.state.message);
+					}
+				});
+		};
+
+		vm.onDecline = function (id) {
+			console.log('decline id: '+id);
+			http.get('private/dashboard/notifications/decline/'+id)
+				.then(function (res) {
+					blockUI.stop();
+					if (res.state) {
+						alertService.add(0, res.state.message);
+					}
+				});
+		};
 
 	});
