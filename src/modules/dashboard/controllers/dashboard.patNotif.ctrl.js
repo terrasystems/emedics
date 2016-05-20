300b313e4d6c2fdd5a8b4e23de85e1f77ce451d6
domino55 <domino55@terrasystems.eu>
2016-05-20 13:44:49 +0300

    T781 notif refresh

--- src/modules/dashboard/controllers/dashboard.patNotif.ctrl.js
+++ src/modules/dashboard/controllers/dashboard.patNotif.ctrl.js
@@ -7,41 +7,44 @@ angular.module('modules.dash')
 		var vm = this;
 		vm.UnreadNotifications = [];
 		vm.searchnotif = '';
+        //
+		//vm.UnreadNotifications = [
+		//{
+		//	'id': '1212',
+		//	'date': new Date(),
+		//	'readtype': 'true',
+		//	'type': '1',
+		//	'title': 'Test Title',
+		//	'text': 'Test Text',
+		//	'fromUser': {id: '4444', username: 'Test #1'},
+		//	'toUser': {id: '333'},
+		//	'userForm': '2222'
+		//},
+		//{
+		//	'id': '1818',
+		//	'date': new Date(),
+		//	'readtype': 'true',
+		//	'type': '1',
+		//	'title': 'Test Title #2',
+		//	'text': 'Test Text #2',
+		//	'fromUser': {id: '333', username: 'Test #2'},
+		//	'toUser': {id: '4444'},
+		//	'userForm': '2222'
+		//}
+		//];
 
-		vm.UnreadNotifications = [
-		{
-			'id': '1212',
-			'date': new Date(),
-			'readtype': 'true',
-			'type': '1',
-			'title': 'Test Title',
-			'text': 'Test Text',
-			'fromUser': {id: '4444', username: 'Test #1'},
-			'toUser': {id: '333'},
-			'userForm': '2222'
-		},
-		{
-			'id': '1818',
-			'date': new Date(),
-			'readtype': 'true',
-			'type': '1',
-			'title': 'Test Title #2',
-			'text': 'Test Text #2',
-			'fromUser': {id: '333', username: 'Test #2'},
-			'toUser': {id: '4444'},
-			'userForm': '2222'
-		}
-		];
-
-		http.post('private/dashboard/notifications', initParamsPOST.params)
-			.then(function (res) {
-				blockUI.stop();
-				if  (res.result) {
-					if  (angular.isArray(res.result) && res.result.length>0) {
-						vm.UnreadNotifications = res.result;
+		vm.onRefreshNotif = function() {
+			http.post('private/dashboard/notifications', initParamsPOST.params)
+				.then(function (res) {
+					blockUI.stop();
+					if (res.result) {
+						//if (angular.isArray(res.result) && res.result.length > 0) {
+							vm.UnreadNotifications = res.result;
+						//}
 					}
-				}
-			});
+				});
+		};
+		vm.onRefreshNotif();
 
 		vm.onAccept = function (id) {
 			console.log('accept id: '+id);
@@ -51,6 +54,7 @@ angular.module('modules.dash')
 					if (res.state) {
 						alertService.add(0, res.state.message);
 					}
+					vm.onRefreshNotif();
 				});
 		};
 
@@ -62,6 +66,7 @@ angular.module('modules.dash')
 					if (res.state) {
 						alertService.add(0, res.state.message);
 					}
+					vm.onRefreshNotif();
 				});
 		};
 
