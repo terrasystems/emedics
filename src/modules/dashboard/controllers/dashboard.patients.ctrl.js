a0e492a9fc070268f1874fb5bf1d921bad926a5c
Valik Novikov <walian19947@gmail.com>
2016-05-23 11:38:33 +0300

    add accordion to Patient tab

--- src/modules/dashboard/controllers/dashboard.patients.ctrl.js
+++ src/modules/dashboard/controllers/dashboard.patients.ctrl.js
@@ -5,4 +5,22 @@ angular.module('modules.dash')
 	.controller('patientsCtrl', function(){
 		 var vm = this;
 		console.log('patientsCtrl !!!!!');
+
+		 vm.patients=[{
+			  FormName:'F1',
+			 id:'!!!',
+			 name:'Petya',
+			 number:'1'
+		 },{
+			 FormName:'F1',
+			 id:'!!!',
+			 name:'Igor',
+			 number:'1'
+		 },
+			 {
+				 FormName:'F1',
+				 id:'!!!',
+				 name:'Vasya',
+				 number:'1'
+			 }];
 	});
