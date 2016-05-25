2579326a5cc590f0fb18b0fd39443ac83ba0c585
Valik Novikov <walian19947@gmail.com>
2016-05-25 16:30:35 +0300

    fixed "remove button" on Patients tab

--- src/modules/dashboard/controllers/dashboard.patients.ctrl.js
+++ src/modules/dashboard/controllers/dashboard.patients.ctrl.js
@@ -2,80 +2,81 @@
 /*jshint -W117, -W097, -W116*/
 
 angular.module('modules.dash')
+
 	.controller('patientsCtrl', function($scope, http, blockUI, initParamsPOST, $state, alertService){
 		var vm = this;
 		vm.searchref = '';
 
-		vm.patients = [
-		{
-			'id': '32',
-			'email': 'tggg1@testmail.com',
-			'phone': '555-55-55',
-			'name': 'Patient #100',
-			'forms': [
-				{
-				'id': '32',
-				'data': null,
-				'blank': {
-					'body': null,
-					'name': 'Blank Name',
-					'type': '1',
-					'descr': 'Descr1',
-					'category': 'main Categ',
-					'number': 'F1'
-				},
-				'active': 'true'
-				},
-				{
-					'id': '33',
-					'data': null,
-					'blank': {
-						'body': null,
-						'name': 'Blank Name #2',
-						'type': '2',
-						'descr': 'Descr2',
-						'category': 'main Categ',
-						'number': 'F2'
-					},
-					'active': 'true'
-				}
-			]
-		},
-		{
-			'id': '3002',
-			'email': 'aaaaa2@testmail.com',
-			'phone': '555-55-55',
-			'name': 'Patient #2000',
-			'forms': [
-			{
-				'id': '320',
-				'data': null,
-				'blank': {
-					'body': null,
-					'name': 'Blank Name',
-					'type': '1',
-					'descr': 'Descr1',
-					'category': 'main Categ',
-					'number': 'F1'
-				},
-				'active': 'true'
-			},
-			{
-				'id': '33',
- 				'data': null,
-				'blank': {
-					'body': null,
-					'name': 'Blank Name #2',
-					'type': '2',
-					'descr': 'Descr2',
-					'category': 'main Categ',
-					'number': 'F2'
-				},
-				'active': 'true'
-			}
-			]
-		}
-		];
+		//vm.patients = [
+		//{
+		//	'id': '32',
+		//	'email': 'tggg1@testmail.com',
+		//	'phone': '555-55-55',
+		//	'name': 'Patient #100',
+		//	'forms': [
+		//		{
+		//		'id': '32',
+		//		'data': null,
+		//		'blank': {
+		//			'body': null,
+		//			'name': 'Blank Name',
+		//			'type': '1',
+		//			'descr': 'Descr1',
+		//			'category': 'main Categ',
+		//			'number': 'F1'
+		//		},
+		//		'active': 'true'
+		//		},
+		//		{
+		//			'id': '33',
+		//			'data': null,
+		//			'blank': {
+		//				'body': null,
+		//				'name': 'Blank Name #2',
+		//				'type': '2',
+		//				'descr': 'Descr2',
+		//				'category': 'main Categ',
+		//				'number': 'F2'
+		//			},
+		//			'active': 'true'
+		//		}
+		//	]
+		//},
+		//{
+		//	'id': '3002',
+		//	'email': 'aaaaa2@testmail.com',
+		//	'phone': '555-55-55',
+		//	'name': 'Patient #2000',
+		//	'forms': [
+		//	{
+		//		'id': '320',
+		//		'data': null,
+		//		'blank': {
+		//			'body': null,
+		//			'name': 'Blank Name',
+		//			'type': '1',
+		//			'descr': 'Descr1',
+		//			'category': 'main Categ',
+		//			'number': 'F1'
+		//		},
+		//		'active': 'true'
+		//	},
+		//	{
+		//		'id': '33',
+ 		//		'data': null,
+		//		'blank': {
+		//			'body': null,
+		//			'name': 'Blank Name #2',
+		//			'type': '2',
+		//			'descr': 'Descr2',
+		//			'category': 'main Categ',
+		//			'number': 'F2'
+		//		},
+		//		'active': 'true'
+		//	}
+		//	]
+		//}
+		//];
 
 		vm.refresh = function () {
 			vm.paramsPOST = initParamsPOST.params;
@@ -139,7 +140,11 @@ angular.module('modules.dash')
 			$state.go('main.private.dashboard.abstract.patients.create');
 		};
 
-		vm.onRemove = function (id_) {
+		vm.onRemove = function (id_,$event) {
+			if($event){
+				$event.stopPropagation();
+				$event.preventDefault();
+			}
 			console.log(id_);
 			vm.paramsPOST = initParamsPOST.params;
 			vm.paramsPOST.criteria.list = [];
