74e70feadaa631bbda5feb22e08a6c43a4f4f66d
Valik Novikov <walian19947@gmail.com>
2016-05-26 16:50:25 +0300

    add save function to  Settings

--- src/modules/dashboard/controllers/settingsCtrl.js
+++ src/modules/dashboard/controllers/settingsCtrl.js
@@ -5,5 +5,16 @@ angular.module('modules.dash')
 
 	.controller('settingsCtrl',function($scope,$state){
 		var vm=this;
-
+		vm.InfoChange={
+			id:null,
+			email:'',
+			typeExp:null
+		};
+vm.onSave=function(){
+	vm.InfoChanged={};
+	vm.InfoChanged.id=vm.InfoChange.id;
+	vm.InfoChanged.email=vm.InfoChange.email;
+	vm.InfoChanged.typeExp=vm.InfoChange.typeExp;
+	console.log(vm.InfoChanged);
+};
 	});
