8e56f365cff6ee6f6fd551b19e902a5de49d7429
Valik Novikov <walian19947@gmail.com>
2016-05-31 11:14:18 +0300

    T793  add message to validation in "create Refs"

--- src/modules/dashboard/controllers/dashboard.patRefsAdd.ctrl.js
+++ src/modules/dashboard/controllers/dashboard.patRefsAdd.ctrl.js
@@ -3,55 +3,58 @@
 
 angular.module('modules.dash')
 
-	.controller('patientReferencesAddCtrl', function ($state, localStorageService, initParamsPOST, http, blockUI, $timeout, alertService) {
+	.controller('patientReferencesAddCtrl', function ($translate,$state, localStorageService, initParamsPOST, http, blockUI, $timeout, alertService) {
 		//console.log('..patientReferencesAddCtrl');
 		var vm = this;
 		vm.user = localStorageService.get('userData');
 		vm.paramsPOST = initParamsPOST.params;
+		vm.msg ='incorrect email';
 		vm.model = {};
 		vm.fields = [
-					//{
-					//	className: 'col-md-12',
-					//	key: 'name',
-					//	type: 'input',
-					//	templateOptions: {
-					//		label: 'Name',
-					//		required: false,
-					//		placeholder: 'Enter Name'
-					//	}
-					//},
-					{
-						className: 'col-md-12',
-						key: 'email',
-						type: 'input',
-						validators: {
-							EmailAddress: {
-								expression: function ($viewValue, $modelValue) {
-									var value = $modelValue || $viewValue;
-									return /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$/.test(value);
-								},
-								message: '$viewValue + " is not a valid e-mail address"'
-							}
-						},
-						validation: {
-							messages: {
-								required: function ($viewValue, $modelValue, scope) {
-									return scope.to.label + ' is required'
-								}
-							}
+			//{
+			//	className: 'col-md-12',
+			//	key: 'name',
+			//	type: 'input',
+			//	templateOptions: {
+			//		label: 'Name',
+			//		required: false,
+			//		placeholder: 'Enter Name'
+			//	}
+			//},
 
-						},
-						templateOptions: {
-							type: 'email',
-							required: true,
-							label: 'Email address',
-							placeholder: 'Enter email'
+			{
+				className: 'col-md-12',
+				key: 'email',
+				type: 'input',
+				validators: {
+					EmailAddress: {
+						expression: function ($viewValue, $modelValue) {
+							var value = $modelValue || $viewValue;
+							return /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$/.test(value);
+						}
+
+				}
+				},
+				templateOptions: {
+					type: 'text',
+					required: true,
+					label: $translate.instant('EMAIL'),
+					placeholder: $translate.instant('EMAIL_1')
+				},
+				validation: {
+					messages: {
+						required: function ($viewValue, $modelValue, scope) {
+							return scope.to.label + ' is required';
 						}
 					}
-					];
+				}
+
+			}
+		];
+
 
 		vm.onSubmit = function () {
-			http.post('private/dashboard/'+vm.user.type+'/references/create', vm.model.email)
+			http.post('private/dashboard/' + vm.user.type + '/references/create', vm.model.email)
 				.then(function (res) {
 					blockUI.stop();
 					if (res.state) {
