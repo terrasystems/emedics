0565dabe7fa6384c27b7eed3d703fb1c1aa5a880
domino55 <domino55@terrasystems.eu>
2016-06-08 12:28:07 +0300

    T824 add notification

--- src/modules/dashboard/controllers/dashboard.Catalog.ctrl.js
+++ src/modules/dashboard/controllers/dashboard.Catalog.ctrl.js
@@ -3,7 +3,7 @@
 
 angular.module('modules.dash')
 
-	.controller('CatalogCtrl', function (http, blockUI, alertService, $state) {
+	.controller('CatalogCtrl', function (http, blockUI, alertService, $state, $uibModal) {
 		var vm = this;
 		vm.myForms = [];
 
@@ -25,7 +25,6 @@ angular.module('modules.dash')
 					alertService.add(0, res.state.message);
 					vm.onRefresh();
 				});
-
 		};
 
 		vm.onGoTemplates = function() {
@@ -39,15 +38,45 @@ angular.module('modules.dash')
 			$state.go('main.private.dashboard.abstract.catalog.catalogtemplate', { arr: vm.arr, onCheck: true });
 		};
 
-		vm.onAddTask = function(id_) {
-			var paramsPOST = {template: {id: id_, templateDto: {id: null, body: {sections:[], name: ''}}}};
-			http.post('private/dashboard/tasks/create', paramsPOST)
-				.then(function (res) {
-					blockUI.stop();
-					if  (res.result) {
-						alertService.add(0, res.state.message);
+		vm.onAddTask = function(model) {
+			blockUI.start();
+			//var paramsPOST = {template: {id: id_, templateDto: {id: null, body: {sections:[], name: ''}}}};
+			//http.post('private/dashboard/tasks/create', paramsPOST)
+			//	.then(function (res) {
+			//		blockUI.stop();
+			//		if  (res.result) {
+			//			alertService.add(0, res.state.message);
+			//		}
+			//	});
+			var result = $uibModal.open({
+				templateUrl: 'modules/dashboard/views/modal.addNotif.html',
+				controller: 'modalAddNotifCtrl',
+				controllerAs: 'vm',
+				resolve: {
+					model: function($q) {
+						var deferred = $q.defer();
+						deferred.resolve({data: model});
+						return deferred.promise;
 					}
-				});
+				}
+			}).result;
+		};
+
+	})
+
+
+	.controller('modalAddNotifCtrl', function ($uibModalInstance, model, blockUI) {
+		var vm = this;
+		vm.model = model;
+
+		blockUI.stop();
+
+		vm.ok = function () {
+			$uibModalInstance.close(vm.model);
+		};
+
+		vm.cancel = function cancel() {
+			$uibModalInstance.dismiss('cancel');
 		};
 
 	});
