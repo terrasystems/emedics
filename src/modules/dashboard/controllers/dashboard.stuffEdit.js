da43822e41ef24b578910bf1199ef48058b14e2b
Valik Novikov <walian19947@gmail.com>
2016-06-20 15:24:15 +0300

    T892 add view and ctrl

---
+++ src/modules/dashboard/controllers/dashboard.stuffEdit.js
@@ -0,0 +1,113 @@
+'use strict';
+/*jshint -W117, -W097*/
+
+angular.module('modules.dash')
+	.controller('stuffEditCtrl', function(http, blockUI, alertService, $state, $stateParams,$translate){
+		var vm = this;
+		console.log($stateParams.id);
+console.log('stuff edit ctrl');
+
+		if (!$stateParams.id || $stateParams.id === '' || $stateParams.id === null) {
+			$state.go('main.private.dashboard.abstract.stafs');
+			return;
+		}
+    vm.stuff={};
+
+		vm.StuffFields = [
+			{
+				className: 'col-md-12',
+				key: 'vm.stuff.firstName',
+				type: 'input',
+				templateOptions: {
+					required: false,
+					label: $translate.instant('FIRST_NAME'),
+					placeholder: $translate.instant('FIRST_NAME')
+				},
+				validation: {
+					show: true
+				}
+			},
+			{
+				className: 'col-md-12',
+				key: 'vm.stuff.lastName',
+				type: 'input',
+				templateOptions: {
+					required: false,
+					label: $translate.instant('LAST_NAME'),
+					placeholder: $translate.instant('LAST_NAME')
+				},
+				validation: {
+					show: true
+				}
+			},
+			{
+				className: 'col-md-12',
+				key: 'vm.stuff.birth',
+				type: 'datepicker',
+				templateOptions: {
+					type: 'text',
+					label: $translate.instant('BIRTH_DATE'),
+					datepickerPopup: 'yyyy-MMMM-dd'
+				}
+			},
+			{
+				className: 'col-md-12',
+				key: 'vm.stuff.email',
+				type: 'input',
+				validators: {
+					EmailAddress: {
+						expression: function ($viewValue, $modelValue) {
+							var value = $modelValue || $viewValue;
+							return /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$/.test(value);
+						},
+						message: '$viewValue + $translate.instant("NO_VALID_EMAIL")'
+					}
+				},
+				templateOptions: {
+					type: 'text',
+					required: true,
+					label: $translate.instant('EMAIL'),
+					placeholder: $translate.instant('EMAIL_1')
+				},
+				validation: {
+					show: true,
+					messages: {
+						required: function ($viewValue, $modelValue, scope) {
+							return scope.to.label + ' is required';
+						}
+					}
+				}
+			},
+			{
+				className: 'col-md-12',
+				key: 'vm.stuff.password',
+				type: 'input',
+				templateOptions: {
+					type:'password',
+					required: false,
+					label: $translate.instant('PASSWORD')
+				},
+				validation: {
+					show: true
+				}
+			},
+			{
+				className: 'col-md-12',
+				key: 'vm.stuff.typeExp',
+				type: 'select',
+				templateOptions: {
+					required: false,
+					label: $translate.instant('TYPE_EXPORT'),
+					placeholder: $translate.instant('TYPE_EXPORT'),
+					options: [
+						{name: $translate.instant('PDF'), value: 'PDF'},
+						{name: $translate.instant('HTML'), value: 'HTML'}
+					]
+				},
+				validation: {
+					show: true
+				}
+			}
+		];
+
+	});
