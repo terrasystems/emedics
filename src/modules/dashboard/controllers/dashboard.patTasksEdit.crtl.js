52d4196835ba5fc173f5b6108f497d8b04ce9b84
Valik Novikov <walian19947@gmail.com>
2016-07-19 15:30:12 +0300

    Merge branch '#816/feature-templates' of /Users/valiknovikov/Desktop/emedics-web with conflicts.

--- src/modules/dashboard/controllers/dashboard.patTasksEdit.crtl.js
+++ src/modules/dashboard/controllers/dashboard.patTasksEdit.crtl.js
@@ -1,23 +1,26 @@
 'use strict';
-/*jshint -W117, -W097, -W116, -W089, -W061*/
-
+/*jshint -W117, -W097, -W116*/
 angular.module('modules.dash')
+	.controller('patientTasksEditCtrl', function ($uibModal, http, $q, $stateParams, $state, localStorageService, blockUI,
+												  $scope, alertService, $timeout, $translate, $base64, confirmService, pouch_db, $rootScope,forEditTask) {
 
-	.controller('patientTasksEditCtrl', function (http, $q, $stateParams, $state, localStorageService, blockUI, $scope, alertService, $timeout, $translate, $base64) {
-		//console.log('Type: ' + $stateParams.type + ' id: ' + $stateParams.id + ' patId: ' + $stateParams.patId);
 		if (!$stateParams.type || $stateParams.type === '' || $stateParams.type === null) {
 			$state.go('main.private.dashboard.abstract.tasks');
 			return;
 		}
 
 		var vm = this;
+		vm.user = localStorageService.get('userData');
+		vm.onNeedSave = true;
 
-		if ($stateParams.type == 'tasks') {
+		if ($stateParams.type == 'tasks' || $stateParams.type == 'tasks+') {
 			vm.mainState = 'main.private.dashboard.abstract.tasks';
-		} else {
+		} else if ($stateParams.type == 'patients' || $stateParams.type == 'patients+') {
 			vm.mainState = 'main.private.dashboard.abstract.patients';
+		} else if ($stateParams.type == 'tasksAdmin') {
+			vm.mainState = 'main.private.dashboard.abstract.tasks';
 		}
-     vm.hideButton = $stateParams.type;
+
 		if (!$stateParams.id || $stateParams.id === '' || $stateParams.id === null) {
 			$state.go(vm.mainState);
 			return;
@@ -25,106 +28,48 @@ angular.module('modules.dash')
 			vm.id = $stateParams.id;
 		}
 
-		vm.user = localStorageService.get('userData');
-		if ($stateParams.type == 'tasks') {
-			vm.getUrl = 'private/dashboard/' + vm.user.type + '/forms/' + vm.id;
-			vm.setUrl = 'private/dashboard/' + vm.user.type + '/forms/edit';
+		if ($stateParams.type == 'patients+' || $stateParams.type == 'tasks+') {
+			vm.viewButtons = false;
+			vm.viewButtonSave = false;
+		} else if ($stateParams.type == 'tasksAdmin') {
+			vm.viewButtons = false;
+			vm.viewButtonSave = true;
 		} else {
-			vm.getUrl = 'private/dashboard/docpatients/forms/'+vm.id;
-			vm.setUrl = 'private/dashboard/docpatients/forms/edit';
+			vm.viewButtons = true;
+			vm.viewButtonSave = true;
 		}
 
-		vm.sections = [];
-		vm.options = [];
-		vm.model = [];
-		vm.sectionsName = [];
-		vm.selectedSection = '';
-		vm.selectedKey = '';
+		if ($stateParams.type == 'tasksAdmin') {
+			vm.getUrl = 'private/dashboard/tasks/' + vm.id;
+			vm.setUrl = 'private/dashboard/stuff/event/adminEdit';
+		} else {
+			vm.getUrl = 'private/dashboard/tasks/' + vm.id;
+			vm.setUrl = 'private/dashboard/tasks/edit';
+		}
 
-		vm.onSubmit = onSubmit;
+		vm.data = { sections: [], options: [], model: [], sectionsName: [], selectedSection: '', selectedKey: '', editModel: {} };
 
-		var paramsPOST = {};
-
-		http.get(vm.getUrl, paramsPOST)
-			.then(function (res) {
-				blockUI.stop();
-
-				if ($stateParams.type == 'tasks') {
-					vm.checkArr = (res.result && res.result.blank && res.result.blank.body && res.result.blank.body.sections && res.result.id);
-				} else {
-					vm.checkArr = (res.result && res.result.form && res.result.form.blank && res.result.form.blank.body && res.result.form.blank.body.sections && res.result.form.id);
-				}
-
-				if (vm.checkArr) {
-					vm.model = (res.result.data && res.result.data.sections) ? res.result.data.sections : undefined;
-					vm.formInfo = {};
-					vm.formInfo.id = ($stateParams.type == 'tasks') ?  res.result.id : res.result.form.id;
-					vm.formInfo.category = ($stateParams.type == 'tasks') ? res.result.blank.category : res.result.form.blank.category;
-					vm.formInfo.name =($stateParams.type == 'tasks') ? res.result.blank.name : res.result.form.blank.name;
-					vm.formInfo.number = ($stateParams.type == 'tasks') ? res.result.blank.number : res.result.form.blank.number;
-					vm.formInfo.descr = ($stateParams.type == 'tasks') ? res.result.blank.descr : res.result.form.blank.descr;
-
-					vm.sectionsName = [];
-					vm.sections = [];
-					if ($stateParams.type == 'tasks') {
-						vm.sections = eval($base64.decode(res.result.blank.body.sections));
-						vm.sections.forEach(function (item) {
-							vm.sectionsName.push(Object.keys(item)[0]);
-						});
-					} else {
-						vm.sections = eval($base64.decode(res.result.form.blank.body.sections));
-						vm.sections.forEach(function (item) {
-							vm.sectionsName.push(Object.keys(item)[0]);
-						});
-					}
-
-					if (!vm.model) {
-						vm.model = [];
-						vm.sectionsName.forEach(function (item) {
-							var it = {};
-							it[item] = {};
-							vm.model.push(it);
-						});
-					}
-					vm.selectedSection = vm.sectionsName[0];
-					if (vm.sectionsName.length > 0) {
-						for (var key in  vm.model) {
-							var obj = vm.model[key][Object.keys(vm.model[key])[0]];
-							for (var prop in obj) {
-								if (obj.hasOwnProperty(prop) && prop.indexOf('_DATE') > 0 && obj[prop] !== null) {
-									obj[prop] = new Date(obj[prop]);
-								}
-							}
+		forEditTask.getModel(vm.getUrl, null)
+			.then(function(res) {
+				vm.data = res;
+				vm.originalModel = JSON.stringify(vm.data.model);
+				$scope.$watch('vm.data.selectedSection', function (newValue) {
+					for (var key in vm.data.model) {
+						if (newValue == Object.keys(vm.data.model[key])[0]) {
+							vm.data.selectedKey = key;
 						}
 					}
-
-					$scope.$watch('vm.selectedSection', function (newValue) {
-						for (var key in vm.model) {
-							if (newValue == Object.keys(vm.model[key])[0]) {
-								vm.selectedKey = key;
-							}
-						}
-					});
-				}
-			});
-
-		vm.s = function () {
-			save().then(function () {
-						$state.go('main.private.dashboard.abstract.notifications.addnotification', {
-							id: vm.formInfo.id,
-							name: vm.formInfo.name
-						});
-				}
-			);
-		};
+				});
+		});
 
 		function save() {
+			vm.onNeedSave = false;
 			var deferred = $q.defer();
-			paramsPOST = {};
-			paramsPOST.id = vm.id;
-			paramsPOST.data = {};
-			paramsPOST.data.sections = vm.model;
-			paramsPOST.blank = null;
+			var paramsPOST = {event: {id: vm.id, data: {sections: vm.data.model}}};
+			paramsPOST.event.patient = vm.data.editModel.patient;
+			paramsPOST.event.template = vm.data.editModel.template;
+			paramsPOST.event.fromUser = vm.data.editModel.fromUser;
+			paramsPOST.event.toUser = vm.data.editModel.toUser;
 
 			http.post(vm.setUrl, paramsPOST)
 				.then(function (res) {
@@ -132,6 +77,7 @@ angular.module('modules.dash')
 					deferred.resolve(res);
 					if (res.state) {
 						alertService.add(0, res.state.message);
+
 					}
 				}, function (error) {
 					deferred.reject(error);
@@ -139,12 +85,73 @@ angular.module('modules.dash')
 			return deferred.promise;
 		}
 
-		function onSubmit() {
+		vm.onSave = function () {
 			save().then(function () {
-				$timeout(function () {
-					$state.go(vm.mainState);
-				}, 0);
+				pouch_db.save($rootScope.db, vm.data.formInfo.rawData.template.id, vm.data.formInfo, vm.data.model)
+					.then(function() {
+						alertService.add(0, 'Saved - Ok!');
+						$state.go(vm.mainState);
+					});
 			});
-		}
+		};
+
+		//vm.onSaveDraft = function() {
+			//pouch_db.save($rootScope.db, 'add', vm.data.formInfo, vm.data.model)
+			//	.then(function() {
+			//		alertService.add(0, 'Saved - Ok!');
+			//		$state.go(vm.mainState);
+			//	});
+		//};
+
+		vm.onSend = function() {
+			var config = {
+				templateUrl: 'modules/dashboard/views/modal.addNotif.html',
+				controller: 'modalAddNotifCtrl',
+				controllerAs: 'vm',
+				resolve: {
+					model: function($q) {
+						var deferred = $q.defer();
+						deferred.resolve({data: {task_id: vm.id, obj: vm.data.formInfo}});
+						return deferred.promise;
+					}
+				}
+			};
+
+			save().then(function () {
+				var result = $uibModal.open(config);
+				result.result.then(function () {
+					$state.go(vm.mainState);
+				});
+			});
+
+		};
+
+		vm.onReturn = function () {
+			$state.go(vm.mainState);
+		};
+
+		vm.onCloseTask = function() {
+			confirmService('Close task?')
+				.then(function(res) {
+				vm.onNeedSave = false;
+				http.get('private/dashboard/tasks/close/'+vm.id)
+					.then(function (res) {
+						blockUI.stop();
+						alertService.add(0, res.state.message);
+						vm.onReturn();
+					});
+				});
+		};
+
+		$scope.$on('$destroy', function() {
+			if  (!($stateParams.type == 'patients+' || $stateParams.type == 'tasks+' || !vm.onNeedSave)) {
+				if  (!angular.equals(vm.originalModel,JSON.stringify(vm.data.model))) {
+					confirmService('Save task?')
+						.then(function() {
+							save();
+						});
+					}
+				}
+		});
 
 	});
